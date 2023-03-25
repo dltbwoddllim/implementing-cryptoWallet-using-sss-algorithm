a561d4858cc25744d5c5338880aa988681129388
dltbwoddllim <dltbwoddl@gmail.com>
2023-03-25 15:45:23 +0900

    Mkey develope

--- Mkey.js
+++ Mkey.js
@@ -1,7 +1,9 @@
+import crypto from 'crypto';
+import BN from 'bn.js';
+import util from "util";
+// import bip39 from 'bip39';
 
-const crypto = require('crypto')
-const BN = require("bn.js")
-const bip39 = require('bip39');
+const pbkdf2Async = util.promisify(crypto.pbkdf2);
 
 export class manageKey {
     constructor() {
@@ -10,7 +12,7 @@ export class manageKey {
         this.point_3 = new points(new BN(0), new BN(0));
         this.coEfficient = new BN(0);
         this.privateKey = new BN(0);
-        this.userInput = "";
+        this.userInput = "dladygks112-";
     }
 
 
@@ -21,7 +23,7 @@ export class manageKey {
     }
 
     //generate points that be used in recovering PrivateKey
-    genPoints() {
+    genPoints(num) {
         this.point_1.X = new BN(crypto.randomBytes(32).toString('hex'), 16);
         this.point_2.X = new BN(crypto.createHash('sha256').update(this.userInput).digest('hex'), 16);
         this.point_3.X = new BN(crypto.randomBytes(32).toString('hex'), 16);
@@ -32,45 +34,38 @@ export class manageKey {
 
         return
     }
-    //manage 
-    encryptPoint(point){
-        var RSAPubKey = new crypto.createPublicKey(this.point_2.X .toBuffer);
-        var encryptedPoint = crypto.publicEncrypt(RSAPubKey, point.toBuffer())
-        return encryptedPoint
+
+    async encryptPointAsync(password, dataToEncrypt) {
+        const salt = crypto.randomBytes(32); // generate a random salt
+        const iterations = 100000; // number of iterations
+        const keyLength = 256; // key length in bits
+
+        const derivedKey = await pbkdf2Async(password, salt, iterations, keyLength, 'sha256');
+        const cipher = crypto.createCipher('aes-256-cbc', derivedKey);
+        let encrypted = cipher.update(dataToEncrypt, 'utf8', 'hex');
+        encrypted += cipher.final('hex');
+
+        return { salt, encryptedData: encrypted };
     }
 
-    decryptPoint(encryptedPoint){
-        var RSAPrivKey = new crypto.createPrivateKey(this.point_2.X .toBuffer);
-        var decryptedPoint = crypto.publicDecrypt(RSAPrivKey,encryptedPoint)
-        return decryptedPoint
+    async decryptPointAsync(password, encryptedData) {
+        const { salt, encryptedData: encrypted } = encryptedData;
+        const iterations = 100000; // number of iterations
+        const keyLength = 256; // key length in bits
+
+        const derivedKey = await pbkdf2Async(password, salt, iterations, keyLength, 'sha256');
+        const decipher = crypto.createDecipher('aes-256-cbc', derivedKey);
+        let decrypted = decipher.update(encrypted, 'hex', 'utf8');
+        decrypted += decipher.final('utf8');
+
+        return decrypted;
     }
-    // saveAtLocal() {
-
-    // }
-
-    // saveAtDrive() {
-
-    // }
-
-    // getPointFromLocal() {
-
-    // }
-
-
-    // getPointFromDrive() {
-
-    // }
-
-
-    // getUserInput() {
-
-    // }
 
     recoveryPrivateKey(point_1, point_2) {
         const co = (point_1.Y.sub(point_2.Y)).div(point_1.X.sub(point_2.X))
         this.privateKey = point_1.Y.sub(co.mul(point_1.X))
         return this.privateKey
-    }   
+    }
 }
 
 class points {
--- test.js
+++ test.js
@@ -1,10 +1,26 @@
-import { manageKey } from "./Mkey";
-
+import { manageKey } from "./Mkey.js";
+import crypto from 'crypto';
+import BN from 'bn.js';
 var Mkey = new manageKey()
 var PrivKey = Mkey.genPrivateKey();
+
 Mkey.genPoints();
 console.log(PrivKey);
 console.log(Mkey.recoveryPrivateKey(Mkey.point_1, Mkey.point_2));
 console.log(Mkey.recoveryPrivateKey(Mkey.point_1, Mkey.point_3));
 console.log(Mkey.recoveryPrivateKey(Mkey.point_2, Mkey.point_3));
-console.log(Mkey.encryptPoint(Mkey.point_1))
+
+var encryptedPoint_1= '';
+Mkey.encryptPointAsync(Mkey.point_2.X.toString(),Mkey.point_1.X.toString()).then((result)=>{
+    console.log(result);
+    var userinput = "dladygks112-";
+    var password= new BN(crypto.createHash('sha256').update(userinput).digest('hex'), 16).toString();
+    console.log(password);
+
+    Mkey.decryptPointAsync(password, result).then((result2)=>{
+        console.log(result2);
+        console.log(Mkey.point_1.X.toString())
+        const a = new BN(result2);
+        console.log(a.toString());
+    })
+})
