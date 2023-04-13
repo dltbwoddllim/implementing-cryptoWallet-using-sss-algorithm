e0d13459b6035e527827e0cbae2780794b08dde0
dltbwoddllim <dltbwoddl@gmail.com>
2023-04-13 17:01:06 +0900

    json recovery

--- wallet/src/App.vue
+++ wallet/src/App.vue
@@ -28,7 +28,8 @@ export default {
       initpage: initpage,
       jsonfile: "",
       ethbalance: 0,
-      address: ""
+      address: "",
+      recoveryJsonFile: ""
     }
   },
   methods: {
@@ -49,6 +50,9 @@ export default {
       myMkey.userPoint_xInputGenerateY(this.point_3_x);
       this.initpage = 3
     },
+    inputValuePasstest(){
+      myMkey.userPoint_xInputGenerateY(this.point_3_x);
+    },
     inputPwPass() {
       myencryptItem.setPassword(this.password);
       if (this.initpage == 5) {
@@ -63,6 +67,9 @@ export default {
     },
     onPassword(e) {
       this.password = e.target.value
+    },
+    onrecoveryJsonFile(e) {
+      this.recoveryJsonFile = e.target.value
     },
 
     saveTolocalstorage() {
@@ -102,21 +109,21 @@ export default {
     },
     pointJsonData() {
       const encryptDatas = {
-          salt: myencryptItem.salt,
-          iv: myencryptItem.iv,
-          point_2_Y: "",
-          point_3_X: "",
-          point_3_Y: ""
+        salt: myencryptItem.salt,
+        iv: myencryptItem.iv,
+        point_2_X: "",
+        point_2_Y: "",
+        point_3_Y: ""
       }
-      const points = [myMkey.point_2.Y, myMkey.point_3.X, myMkey.point_3.Y];
+      const points = [myMkey.point_2.X, myMkey.point_2.Y, myMkey.point_3.Y];
       myencryptItem.encryptBatch(points).then((results) => {
         for (var i = 0; i < results.length; i++) {
           console.log(results[i])
           points[i] = new Uint8Array(results[i])
         }
       }).then(() => {
-        encryptDatas['point_2_Y'] = points[0];
-        encryptDatas['point_3_X'] = points[1];
+        encryptDatas['point_2_X'] = points[0];
+        encryptDatas['point_2_Y'] = points[1];
         encryptDatas['point_3_Y'] = points[2];
         const serializedData = JSON.stringify(encryptDatas)
         this.jsonfile = serializedData
@@ -153,7 +160,7 @@ export default {
 
         myencryptItem.iv = uint8Arr1
         myencryptItem.salt = uint8Arr2
-        myencryptItem.decryptBatch([uint8Arr3,uint8Arr4,uint8Arr5]).then((results) => {
+        myencryptItem.decryptBatch([uint8Arr3, uint8Arr4, uint8Arr5]).then((results) => {
           console.log(results);
           myMkey.point_2.Y = new BN(results[0])
           myMkey.point_3.X = new BN(results[1])
@@ -164,10 +171,53 @@ export default {
           myMkey.recoveryPrivateKey(myMkey.point_2, myMkey.point_3);
           console.log(myMkey.recoverykey.toJSON())
           console.log(myMkey.privateKey.toJSON())
-          
+
         })
       })
 
+    },
+    recoveryByJson() {
+      // decrypt part
+      const encryptDatass = JSON.parse(this.recoveryJsonFile);
+      console.log(encryptDatass)
+      var uint8Arr1 = new Uint8Array(Object.keys(encryptDatass.iv).length);
+      var uint8Arr2 = new Uint8Array(Object.keys(encryptDatass.salt).length);
+      var uint8Arr3 = new Uint8Array(Object.keys(encryptDatass.point_2_X).length);
+      var uint8Arr4 = new Uint8Array(Object.keys(encryptDatass.point_2_Y).length);
+      var uint8Arr5 = new Uint8Array(Object.keys(encryptDatass.point_3_Y).length);
+
+      for (var i = 0; i < uint8Arr1.length; i++) {
+        uint8Arr1[i] = encryptDatass.iv[i]
+      }
+      for (var j = 0; j < uint8Arr2.length; j++) {
+        uint8Arr2[j] = encryptDatass.salt[j]
+      }
+      for (var t = 0; t < uint8Arr3.length; t++) {
+        uint8Arr3[t] = encryptDatass.point_2_X[t]
+      }
+      for (var k = 0; k < uint8Arr4.length; k++) {
+        uint8Arr4[k] = encryptDatass.point_2_Y[k]
+      }
+      for (var p = 0; p < uint8Arr5.length; p++) {
+        uint8Arr5[p] = encryptDatass.point_3_Y[p]
+      }
+
+      myencryptItem.iv = uint8Arr1
+      myencryptItem.salt = uint8Arr2
+      myencryptItem.decryptBatch([uint8Arr3, uint8Arr4, uint8Arr5]).then((results) => {
+        console.log(results);
+        myMkey.point_2.X = new BN(results[0])
+        myMkey.point_2.Y = new BN(results[1])
+        myMkey.point_3.Y = new BN(results[2])
+        console.log(myMkey.point_2.X)
+        console.log(myMkey.point_2.Y)
+        console.log(myMkey.point_3.X)
+        console.log(myMkey.point_3.Y)
+        myMkey.recoveryPrivateKey(myMkey.point_2, myMkey.point_3);
+        console.log(myMkey.recoverykey.toJSON())
+        console.log(myMkey.privateKey.toJSON())
+
+      })
     },
     goTohome() {
       this.initpage = 6;
@@ -200,7 +250,7 @@ export default {
   </div>
   <div v-else-if="this.initpage == 2">
     <button @click="genPoints">genPoints</button><br>
-    <input :value="point_3_x" @input="onInput" placeholder="여기에 입력하기"><button @click="inputValuePass">onInput</button><br>
+    <input :value="point_3_x" @input="onInput" placeholder="point3x입력"><button @click="inputValuePass">onInput</button><br>
   </div>
   <div v-else-if="this.initpage == 3">
     <input :value="password" @input="onPassword" placeholder="비밀번호 입력"><br>
@@ -212,10 +262,14 @@ export default {
     <button @click="goTohome">goTohome</button>
   </div>
   <div v-else-if="this.initpage == 5">
-    <input :value="password" @input="onPassword" placeholder="비밀번호 입력"><br>
-    <button @click="inputPwPass">onPassword</button><br>
+    <input :value="point_3_x" @input="onInput" placeholder="point3x입력"><button @click="inputValuePasstest">onInput</button><br>
+    <input :value="password" @input="onPassword" placeholder="비밀번호 입력"><button @click="inputPwPass">onPassword</button><br>
+    <input :value="recoveryJsonFile" @input="onrecoveryJsonFile" placeholder="json 입력"><br>
+    <button @click="recoveryByJson">recoveryByJson</button><br>
     <button @click="goTohome">goTohome</button>
+
   </div>
+
   <div v-else>
     <p>주소 : {{ address }}</p>
     <p>eth : {{ ethbalance }}</p>
